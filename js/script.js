e97466cc3cf817b3cae657d7c49bbe07f7761012
wenchicheng <wenchicheng@outlook.com>
2024-01-16 16:45:50 +0800

    navbar toggler

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.navbar-toggler-icon').css({'background': ''})
+
+$('.navbar-toggler-icon').on('click', function() {
+
+  $(this).toggleClass('active');
+})
+
+// $('.navbar-toggler-icon').on('click', function() {
+//   if (parseInt($(this).css('opacity')) === 1) {
+//     $(this).css({'opacity': 0});
+//     $(this).parent().find('.navbar-toggler-icon::before').css({'opacity': 1});
+//   } else if (parseInt($(this).css('opacity')) === 0) {
+//     $(this).css({'opacity': 1});
+//     $(this).parent().find('.navbar-toggler-icon::before').css({'opacity': 0});
+//   }
+// })
