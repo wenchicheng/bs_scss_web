c79b7e621f9ef88d2f455e578fdcde6f272f00ab
wenchicheng <wenchicheng@outlook.com>
2024-01-23 01:12:46 +0800

    upload
    使用套件
    1.Swiper(輪播)-----使用於 Section01
    網址：https://swiperjs.com/demos
    
    2.animate.css(滑入動畫)-----使用於 Section01
    網址：https://animate.style/
    -fadeInDown
    -fadeInRight
    
    3.GSAP(滑鼠)-----使用於滑鼠事件監聽效果
    網址：https://gsap.com/

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # bs_scss_web
+
+***使用套件
+1.Swiper(輪播)-----使用於 Section01
+網址：https://swiperjs.com/demos
+
+2.animate.css(滑入動畫)-----使用於 Section01
+網址：https://animate.style/
+-fadeInDown
+-fadeInRight
+
+3.GSAP(滑鼠)-----使用於滑鼠事件監聽效果
+網址：https://gsap.com/
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* toggler-icon======================================== */
 $('.navbar-toggler-icon').css({'background': ''})
 $('.navbar-toggler-icon').on('click', function() {
 
@@ -6,15 +7,31 @@ $('.navbar-toggler-icon').on('click', function() {
 
 
 
-// $('.navbar-toggler-icon').on('click', function() {
-//   if (parseInt($(this).css('opacity')) === 1) {
-//     $(this).css({'opacity': 0});
-//     $(this).parent().find('.navbar-toggler-icon::before').css({'opacity': 1});
-//   } else if (parseInt($(this).css('opacity')) === 0) {
-//     $(this).css({'opacity': 1});
-//     $(this).parent().find('.navbar-toggler-icon::before').css({'opacity': 0});
-//   }
-// })
+
+/* swiper===============================================*/
+    /* 
+    兩個參數：
+    第一參數：抓目標，通常是id
+    第二參數:選項物件(option object,{}) 主要設定套件的屬性
+    */
+    const swiper01 = new Swiper("#swiper01", {
+      loop: true,     //無限循環
+      speed: 1500,    //動畫轉場時間
+      autoplay: {
+        delay: 5000   //自動撥放，幾秒後下一張
+      },
+      mousewheel: true,//滾輪
+
+
+      // 啟用上一張、下一張功能
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    });
+
+
 /* GSAP================================================ */
 
     /* 滑鼠效果----------------------  */
